c514a4e45a1cdd85bd8feae44d9bf35318114bbd
Israel <israelfernandezamaro@gmail.com>
2023-04-30 13:53:45 -0700

    BTC_all_time updated, scraper.py updated.

--- ohlcv_scraper.py
+++ ohlcv_scraper.py
@@ -7,7 +7,7 @@ from glob import glob
 import json
 import time
 from cryptocmd import CmcScraper
-
+import os
 
 
 # initialise scraper without time interval
@@ -27,19 +27,19 @@ df = scraper.get_dataframe()
 print(df)
 
 
-#scrape price and sector from coin market cap
-import requests
-from bs4 import BeautifulSoup
+# #scrape price and sector from coin market cap
+# import requests
+# from bs4 import BeautifulSoup
 
-def get_cmc_data(ticker):
-    url = f"https://coinmarketcap.com/currencies/{ticker}/"
-    page = requests.get(url)
-    soup = BeautifulSoup(page.content, "html.parser")
-    price = soup.find("span", class_="cmc-details-panel-price__price").get_text().strip()
-    sector = soup.find("div", class_="cmc-details-panel-sector").get_text().strip()
-    return {"ticker": ticker, "price": price, "sector": sector}
+# def get_cmc_data(ticker):
+#     url = f"https://coinmarketcap.com/currencies/{ticker}/"
+#     page = requests.get(url)
+#     soup = BeautifulSoup(page.content, "html.parser")
+#     price = soup.find("span", class_="cmc-details-panel-price__price").get_text().strip()
+#     sector = soup.find("div", class_="cmc-details-panel-sector").get_text().strip()
+#     return {"ticker": ticker, "price": price, "sector": sector}
 
-tickers = ["bitcoin"]
-data = [get_cmc_data(ticker) for ticker in tickers]
-#print(data)
+# tickers = ["bitcoin"]
+# data = [get_cmc_data(ticker) for ticker in tickers]
+# print(data)
 
